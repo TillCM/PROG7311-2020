a97f40cd3a64431e046244205840b8acdb1952e3
Sarina Till <ctill@varsitycollege.co.za>
2020-04-02 10:51:11 +0200

    uploading API project

---
+++ VCVIDERP/VCVIDERP/Controllers/WeightController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VCVIDERP.Factory;
+using VCVIDERP.LogicManager;
+using VCVIDERP.Models;
+
+
+namespace VCVIDERP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeightController : ControllerBase
+    {
+  
+        [HttpGet]
+        public List<Course> Get()
+        {
+
+            CourseFactory courseFactory = new CourseFactory();
+            ICourseManager courseManager = courseFactory.getCourse("BCOM");
+            List<Course> bcadReturnedData = courseManager.getCourseData();
+            return bcadReturnedData;
+
+        }
+    }
+
+}
