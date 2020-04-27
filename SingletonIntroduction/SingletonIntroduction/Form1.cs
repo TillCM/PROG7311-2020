ebdcd2fdef79e2355e586000aa2306e77b4bcad6
Sarina Till <ctill@varsitycollege.co.za>
2020-04-27 22:07:54 +0200

    pusing singleton

---
+++ SingletonIntroduction/SingletonIntroduction/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingletonIntroduction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Singleton test = Singleton.GetInstance;
+            test.displayClass("test");
+            Singleton test2 = Singleton.GetInstance;
+            test.displayClass("test2");
+            Singleton test3 = Singleton.GetInstance;
+        }
+    }
+}
---
+++ SingletonIntroduction/SingletonIntroduction/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SingletonIntroduction
+{
+    
+    class Singleton
+    {
+        private static int count;
+        private static Singleton instance = null;
+
+        public static Singleton GetInstance
+        {
+            get 
+            {
+                if (instance == null)
+         
+                    instance = new Singleton();
+                    return instance;
+            }
+        }
+
+       private Singleton()
+        {
+            count++;
+            MessageBox.Show("I have instantiated this class " + count + " times");
+        }
+
+        public void displayClass (string value)
+        {
+            MessageBox.Show(" I was instantiated in the  " + value + "class");
+        }
+    }
+}
