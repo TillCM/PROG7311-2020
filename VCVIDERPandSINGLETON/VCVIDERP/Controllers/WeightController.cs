c08efb6611c52c474f2df71df6747c5f192e7f02
Sarina Till <ctill@varsitycollege.co.za>
2020-05-07 13:59:37 +0200

    Pusing Singleton

---
+++ VCVIDERPandSINGLETON/VCVIDERP/Controllers/WeightController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VCVIDERP.Factory;
+using VCVIDERP.LogicManager;
+using VCVIDERP.Models;
+
+
+namespace VCVIDERP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeightController : ControllerBase
+    {
+  
+        [HttpGet]
+        public List<Course> Get(string courseParemeter="BCAD")
+        {
+
+            CourseFactory courseFactory = new CourseFactory();
+            ICourseManager courseManager = courseFactory.getCourse(courseParemeter);
+            List<Course> bcadReturnedData = courseManager.getCourseData();
+            return bcadReturnedData;
+
+        }
+    }
+
+}
